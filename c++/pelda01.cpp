bfcfe9a16a694545ad4b855d0ea9962e0846a846
zsoltkir1 <tarjanzsolt2@hotmail.com>
2017-09-13 18:53:52 +0200

    1. example(my first vector) updated

--- c++/pelda01.cpp
+++ c++/pelda01.cpp
@@ -11,16 +11,28 @@ int main(){
 	vec2 vecB=vec2(8.0,2.0);
 	vec2 vecC=vec2(3.0,6.0);
 	vec2 vecD=vec2(3.0,6.0);
+	
+	printMathObject(vecB,"B: ");
+	printMathObject(vecC,"C: ");
+	printMathObject(vecD,"D: ");
 
 	printMathObject(-vecA,"negated vector: ");
 	
-	printf("Equal %s\n",vecC == vecD ? "True" : "False");
-	printf("Equal %s\n",vecC == vecA ? "True" : "False");
-	printf("Not Equal %s\n",vecC != vecA ? "True" : "False");
+	printf("Equal %s\n",vecC == vecD ? "True " : "False ");
+	printf("Equal %s\n",vecC == vecA ? "True " : "False ");
+	printf("Not Equal %s\n",vecC != vecA ? "True " : "False ");
 	
-	printMathObject(vecA + vecB,"Osszeg");
-	printMathObject(vecC - vecB,"Kulombseg");
-	printMathObject(vecB - vecC,"Kulombseg");
+	printMathObject(vecA + vecB,"Osszeg ");
+	printMathObject(vecC - vecB,"Kulombseg ");
+	printMathObject(vecB - vecC,"Kulombseg ");
+	
+	printMathObject(vecA + 5.0,"Osszeg ");
+	printMathObject(vecC * 10,"Szorzat ");
+	printMathObject(vecB - 2.0,"Kulombseg ");
+	printMathObject(vecD / 2.0,"Osztas ");
+	
+	vecA *=2;
+	printMathObject(vecA, "A duplája");
 	
 	system("PAUSE");
 	return 0;
