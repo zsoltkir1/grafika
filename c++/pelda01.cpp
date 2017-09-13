0ece8d03f1dbec873f21e19ebd18f367f4607468
zsoltkir1 <tarjanzsolt2@hotmail.com>
2017-09-13 18:35:01 +0200

    1. example(my first vector) updates

--- c++/pelda01.cpp
+++ c++/pelda01.cpp
@@ -5,5 +5,9 @@ using namespace std;
 int main(){
 	vec2 vecA = vec2(5.0,10.0);
 	printMathObject(vecA,"my first vector: ");
+	
+	printf("coordinates: %f - %f\n",vecA[0],vecA[1]);
+	
+	system("PAUSE");
 	return 0;
 }
