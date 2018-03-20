7acde3303d1db592a9432de11e7b0466dcadcdcf
zsoltkir1 <tarjanzsolt2@hotmail.com>
2018-03-20 01:10:43 +0100

    szenvedés...

--- c++/beadando004.cpp
+++ c++/beadando004.cpp
@@ -9,6 +9,15 @@ std::vector<vec2> points;
 GLint dragged = -1;
 
 GLint Round(GLfloat n) { return (GLint)(n + 0.5); }
+float t[4] = {-1.0,0.0,1.0,2.0};
+vec4 mvectors[4] = {{pow(t[0],3.0),pow(t[0],2.0),t[0],1.0},{pow(t[1],3.0),pow(t[1],2.0),t[1],1.0},{pow(t[2],3.0),pow(t[2],2.0),t[2],1.0},{pow(t[3],3.0),pow(t[3],2.0),t[3],1.0}};
+mat4 mi = {mvectors[0],mvectors[1],mvectors[2],mvectors[3]};
+mat4 m = transpose(inverse(mi));
+bool go = false;
+GLint keyStates[256];
+vec4 derivaltvec[4]={{3*pow(t[0],2.0),2*t[0],1.0,0.0},{3*pow(t[1],2.0),2*t[1],1.0,0.0},{3*pow(t[2],2.0),2*t[2],1.0,0.0},{3*pow(t[3],2.0),2*t[3],1.0,0.0}};
+mat4 erintov ={derivaltvec[0],derivaltvec[1],derivaltvec[2],derivaltvec[3]};
+mat4 erinto = transpose(inverse(erintov));
 
 void init() {
 	glClearColor(1.0, 1.0, 1.0, 0.0);
@@ -16,24 +25,61 @@ void init() {
 	gluOrtho2D(0.0, winWidth, 0.0, winHeight);
 	glShadeModel(GL_FLAT);
 	glEnable(GL_POINT_SMOOTH);
-	glEnable(GL_LINE_STIPPLE);
+	//glEnable(GL_LINE_STIPPLE);
 	glPointSize(10.0);
 	glLineWidth(5.0);
 	glLineStipple(1, 0xFF00);
 }
 
+void palcikaember() {
+	GLfloat j;
+	//mat24 G={points[0],points[2],points[3],points[1]-points[0]};
+	mat24 G={points[0],points[1],points[2],points[3]};
+	mat24 C = G*m;
+	//vec4 derivaltmatrix = {3*pow(t[2],2.0),2*t[2],1,0};
+	//vec2 e2 = C*derivaltmatrix;
+	glColor3f(0.0, 1.0, 1.0);
+	glBegin(GL_LINE_STRIP);
+		for (j = -1.0; j < 2.0; j=j+0.01){
+			vec4 derivaltmatrix = {3*pow(j,2.0),2*j,1,0};
+			//C=C*derivaltmatrix;
+			vec4 thirdvector = {{pow(j,3)},{pow(j,2)},{j},{1}};
+			vec2 qt = C*derivaltmatrix;
+			vec2 qt2 = C*thirdvector;
+			glVertex2f(-qt.y, qt.x);			
+		}
+		glEnd();
+}
+
 void display() {
 	GLint i;
+	GLfloat j;
 	glClear(GL_COLOR_BUFFER_BIT);
+	
 
-	glColor3f(0.0, 1.0, 0.0);
+	
 
     if (points.size()==4){
-        glBegin(GL_LINE_STRIP);
+		mat24 G={points[0],points[1],points[2],points[3]};
+		mat24 C = G*m;
+		//vec4 derivaltmatrix = {3*pow(t[2],2.0),2*t[2],1,0};
+		//vec2 e2 = C*derivaltmatrix;
+		palcikaember();
+		glColor3f(0.0, 1.0, 0.0);
+		glBegin(GL_LINE_STRIP);
+		for (j = -1.0; j < 2.0; j=j+0.01){
+			vec4 thirdvector = {{pow(j,3)},{pow(j,2)},{j},{1}};
+			vec2 qt = C*thirdvector;
+			glVertex2f(qt.x, qt.y);
+		}
+		glEnd();
+		
+        /*glBegin(GL_LINE_STRIP);
         for (i = 0; i < points.size(); i++)
             glVertex2f(points[i].x, points[i].y);
-        glEnd();
+        glEnd();*/
     }
+	
 
 	glColor3f(1.0, 0.0, 0.0);
 
@@ -47,7 +93,7 @@ void display() {
 	glutSwapBuffers();
 }
 
-/*GLint getActivePoint1(vec2 p[], GLint size, GLint sens, GLint x, GLint y) {
+GLint getActivePoint1(std::vector<vec2> p, GLint size, GLint sens, GLint x, GLint y) {
 	GLint i, s = sens * sens;
 	vec2 P = { (float)x, (float)y };
 
@@ -55,7 +101,7 @@ void display() {
 		if (dist(p[i], P) < s)
 			return i;
 	return -1;
-}*/
+}
 
 /*GLint getActivePoint2(vec2 *p, GLint size, GLint sens, GLint x, GLint y) {
 	GLint i;
@@ -66,26 +112,42 @@ void display() {
 }*/
 
 void processMouse(GLint button, GLint action, GLint xMouse, GLint yMouse) {
-	//GLint i;
+	GLint i;
     if (button == GLUT_LEFT_BUTTON && action == GLUT_DOWN && points.size()<4){
         vec2 pont={float(xMouse), winHeight - float(yMouse)};
         points.push_back(pont);
     }
-	/*if (button == GLUT_LEFT_BUTTON && action == GLUT_DOWN)
+	if (button == GLUT_LEFT_BUTTON && action == GLUT_DOWN && points.size()==4)
 		if ((i = getActivePoint1(points, 4, 8, xMouse, winHeight - yMouse)) != -1)
 			dragged = i;
-	if (button == GLUT_LEFT_BUTTON && action == GLUT_UP)
-		dragged = -1;*/
+	if (button == GLUT_LEFT_BUTTON && action == GLUT_UP && points.size()==4)
+		dragged = -1;
 }
 
-/*void processMouseActiveMotion(GLint xMouse, GLint yMouse) {
+void processMouseActiveMotion(GLint xMouse, GLint yMouse) {
 	GLint i;
 	if (dragged >= 0) {
 		points[dragged].x = xMouse;
 		points[dragged].y = winHeight - yMouse;
 		glutPostRedisplay();
 	}
-}*/
+}
+
+void keyPressed(unsigned char key, int x, int y) {
+    keyStates[key] = 1;
+}
+
+void keyUp(unsigned char key, int x, int y) {
+    keyStates[key] = 0;
+}
+
+void keyOperations() {
+    if (keyStates['n'] ) { 
+        if (go==false){
+            go=true;
+        }       
+	}
+}
 
 void update(int n)
 {   
@@ -104,7 +166,7 @@ int main(int argc, char** argv) {
 	init();
 	glutDisplayFunc(display);
 	glutMouseFunc(processMouse);
-	//glutMotionFunc(processMouseActiveMotion);
+	glutMotionFunc(processMouseActiveMotion);
     glutTimerFunc(5, update, 0);
 	glutMainLoop();
 	return 0;
