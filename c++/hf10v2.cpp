28b2f2e04c930cadf6d9355bfe9f25b93132a490
zsoltkir1 <tarjanzsolt2@hotmail.com>
2018-05-07 22:09:16 +0200

    tizedik beadandó elkészítve

--- c++/hf10v2.cpp
+++ c++/hf10v2.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <math.h>
 #include <algorithm>
+#include <iostream>
 
 struct lapok {
 	vec3 sulypont,normal;
@@ -10,18 +11,19 @@ struct lapok {
 	int lappontok[4];
 };
 
-std::vector<vec3> torus;
-std::vector<lapok> torusLapok;
-std::vector<vec4> transzormaltTorus;
-std::vector<vec2> torusDone;
+std::vector<vec3> Vaza;
+std::vector<lapok> VazaLapok;
+std::vector<vec4> transzormaltVaza;
+std::vector<vec2> VazaDone;
 
 mat4 forgatas, w2v, vetites, coordinateTraszfMatrix,rotatedZ, projection ,scaled, alpha;
 float beta = 0.0, lepeskozfelle = 0.05,lepeskozjobbrabalra = 0.1,hengersugar = 10.0, alfa = 0.0;
 float centrumpont = 20.0;
-vec3 eye = { 0.0, 0.0, 0.0 }, target = { 0,0,0 }, upVector = { 0,0,1 }, Z, X, Y;
+vec3 eye = { 0.0, 0.0, 0.0 }, target = { 0,0,0 }, upVector = { 0,1,0 }, Z, X, Y;
 GLsizei winWidth = 1280, winHeight = 720;
-GLfloat kezdoPoziX = winWidth / 2 -100, kezdoPoziY = winHeight / 2 -100, kezdoSzelesseg = 200.0f, kezdoMagassag = 200.0f;
+GLfloat kezdoPoziX = winWidth / 2 -240, kezdoPoziY = winHeight / 2 -360, kezdoSzelesseg = 480.0f, kezdoMagassag = 480.0f;
 float R = 1.5, r = 0.5;
+float magassag=1;
 
 bool tavolsagszerint(lapok a, lapok b) {
 
@@ -37,8 +39,10 @@ void cameraTranszformacio() {
 }
 
 void cameraSzamol() {
-	eye.y = hengersugar * sin(degToRad(alfa));
+	//eye.y = hengersugar * sin(degToRad(alfa));
+    eye.y = magassag;
 	eye.x = hengersugar * cos(degToRad(alfa));
+    eye.z = -1*hengersugar * sin(degToRad(alfa));
 }
 
 void initMatrices() {
@@ -47,7 +51,7 @@ void initMatrices() {
 	w2v = windowToViewport3(vec2(-1.0f, -1.0f), vec2(2.0f, 2.0f), vec2(kezdoPoziX, kezdoPoziY), vec2(kezdoSzelesseg, kezdoMagassag)); 
 	//w2v mátrix(windows kordináta, window szélesség magasság,viewport helye, viewport szélessége magasséga)
 }
-vec3 fenyVektor = { 0.0,0.0,1.0 };
+vec3 fenyVektor = { 1.0,0.0,0.0 };
 
 int factorial(int f) {
 	int factorial = 1;
@@ -60,73 +64,85 @@ int factorial(int f) {
 vec3 b_p[6] = {
 	{ 0.30f, 0.0f,0.0f }, 
 	{ 1.10f, 0.20f ,0.0f },
-	{ 1.10f, 0.90f,0.0f },
-	{ 0.15f, 0.80f ,0.0f },
+	{ 0.70f, 0.90f,0.0f },
+	{ 0.00f, 0.80f ,0.0f },
 	{ 0.15f, 1.60f ,0.0f },
 	{ 0.45f, 1.60f,0.0f }
 };
+/*vec3 b_p[] = {
+    { 0.15f, 0.0f,0.0f }, 
+	{ 0.55f, 0.10f ,0.0f },
+	{ 0.55f, 0.45f,0.0f },
+	{ 0.00f, 0.40f ,0.0f },
+	{ 0.10f, 0.80f ,0.0f },
+	{ 0.25f, 0.80f,0.0f }
+};*/
+
 
 //(factorial(n) / (factorial(i-bernsteinStart)*factorial(n - (i-bernsteinStart)))
 
-float b_x(float t){
+float bX(float t){
     vec3 qVec;
     int n = 5;
 
 	qVec = {};
 	for (int i = 0; i <= n; i++)
 	{
-		//qVec += under(n, i) * pow(t, i) * pow(1 - t, n - i); * b_p[i];
 		qVec += (factorial(n)/(factorial(i)*factorial(n-i))) * pow(t, i) * pow(1 - t, n - i) * b_p[i];
 	}
     return qVec.x;
 }
 
-float b_y(float t){
+float bY(float t){
     vec3 qVec;
     int n = 5;
 
     qVec = {};
     for (int i = 0; i <= n; i++)
     {
-        //qVec += under(n, i) * pow(t, i) * pow(1 - t, n - i); * b_p[i];
 		qVec += (factorial(n)/(factorial(i)*factorial(n-i))) * pow(t, i) * pow(1 - t, n - i) * b_p[i];
     }
     return qVec.y;
 }
 
 
-void initTorus() {
-	torus.clear();
+void initVaza() {
+	Vaza.clear();
 	/*for (double phi = 0; phi < two_pi(); phi += degToRad(18.0)) {
 		for (double theta = 0; theta < two_pi(); theta += degToRad(18.0)) {
-			torus.push_back(vec3((R + r*cos(phi))*cos(theta), (R + r*cos(phi))*sin(theta), r*sin(phi)));
-			torus.push_back(vec3((R + r*cos(phi + degToRad(18.0)))*cos(theta), (R + r*cos(phi + degToRad(18.0)))*sin(theta), r*sin(phi + degToRad(18.0))));
-			torus.push_back(vec3((R + r*cos(phi + degToRad(18.0)))*cos(theta + degToRad(18.0)), (R + r*cos(phi + degToRad(18.0)))*sin(theta + degToRad(18.0)), r*sin(phi + degToRad(18.0))));
-			torus.push_back(vec3((R + r*cos(phi))*cos(theta + degToRad(18.0)), (R + r*cos(phi))*sin(theta + degToRad(18.0)), r*sin(phi)));
+			Vaza.push_back(vec3((R + r*cos(phi))*cos(theta), (R + r*cos(phi))*sin(theta), r*sin(phi)));
+			Vaza.push_back(vec3((R + r*cos(phi + degToRad(18.0)))*cos(theta), (R + r*cos(phi + degToRad(18.0)))*sin(theta), r*sin(phi + degToRad(18.0))));
+			Vaza.push_back(vec3((R + r*cos(phi + degToRad(18.0)))*cos(theta + degToRad(18.0)), (R + r*cos(phi + degToRad(18.0)))*sin(theta + degToRad(18.0)), r*sin(phi + degToRad(18.0))));
+			Vaza.push_back(vec3((R + r*cos(phi))*cos(theta + degToRad(18.0)), (R + r*cos(phi))*sin(theta + degToRad(18.0)), r*sin(phi)));
 		}
 	}*/
 	float delta = 0.1;
-	for( float theta = 0; theta < two_pi(); theta += delta){
-            for(float bt = 0; bt <= 1; bt += delta)
+    
+	for( float theta = 0; theta < two_pi(); theta += delta*3){
+        Vaza.push_back(vec3(cos(theta) * bX(0), bY(0), -sin(theta) * bX(0)));
+        Vaza.push_back(vec3(cos(theta + delta*3) * bX(0), bY(0), -sin(theta + delta*3) * bX(0)));
+        Vaza.push_back(vec3(0, 0, 0));
+        Vaza.push_back(vec3(0, 0, 0));
+        for(float bt = 0; bt <= 1; bt += delta)
             {
-                torus.push_back(vec3(cos(theta) * b_x(bt), b_y(bt), -sin(theta) * b_x(bt)));
-                torus.push_back(vec3(cos(theta + delta) * b_x(bt), b_y(bt), -sin(theta + delta) * b_x(bt)));
-                torus.push_back(vec3(cos(theta + delta) * b_x(bt + delta), b_y(bt + delta), -sin(theta + delta) * b_x(bt + delta)));
-                torus.push_back(vec3(cos(theta) * b_x(bt + delta), b_y(bt + delta), -sin(theta) * b_x(bt + delta)));
+                Vaza.push_back(vec3(cos(theta) * bX(bt), bY(bt), -sin(theta) * bX(bt)));
+                Vaza.push_back(vec3(cos(theta + delta*3) * bX(bt), bY(bt), -sin(theta + delta*3) * bX(bt)));
+                Vaza.push_back(vec3(cos(theta + delta*3) * bX(bt + delta), bY(bt + delta), -sin(theta + delta*3) * bX(bt + delta)));
+                Vaza.push_back(vec3(cos(theta) * bX(bt + delta), bY(bt + delta), -sin(theta) * bX(bt + delta)));
             }
         }
 }
 
 void initLapok() {
 	
-	torusLapok.clear();
+	VazaLapok.clear();
 
 	lapok temp;
-	for (int i = 0; i < torus.size(); i += 4) {
+	for (int i = 0; i < Vaza.size(); i += 4) {
 		for (int j = 0; j<4;j++){
 			temp.lappontok[j] = i+j;
 		}
-		torusLapok.push_back(temp);
+		VazaLapok.push_back(temp);
 	}
 }
 
@@ -137,26 +153,25 @@ void init() {
 	glShadeModel(GL_FLAT);
 	glEnable(GL_POINT_SMOOTH);
 	glPointSize(10.0);
-	glLineWidth(1.0);
-	initTorus();
+	glLineWidth(1.5);
+	initVaza();
 	initLapok();
-	//initMatrices();
 }
 
 
-void rendezverajzoltoruszt(int i){
-	if ((torusLapok[i].skalaris > 0.0)) {
-				glColor3f(0.0, 1.0, 0.0);
+void rendezverajzolVazat(int i){
+	if ((VazaLapok[i].skalaris > 0.0)) {
+				glColor3f(0.0, 0.0, 0.0);
 				glBegin(GL_LINE_LOOP);
 				for (int j = 0; j < 4; j++) {
-					glVertex2f(torusDone[torusLapok[i].lappontok[j]].x, torusDone[torusLapok[i].lappontok[j]].y);
+					glVertex2f(VazaDone[VazaLapok[i].lappontok[j]].x, VazaDone[VazaLapok[i].lappontok[j]].y);
 				}
 				glEnd();
 				
-				glColor3f(torusLapok[i].lapszin, torusLapok[i].lapszin, torusLapok[i].lapszin);
+				glColor3f(VazaLapok[i].lapszin, VazaLapok[i].lapszin, VazaLapok[i].lapszin);
 				glBegin(GL_POLYGON);
 				for (int j = 0; j < 4; j++) {
-					glVertex2f(torusDone[torusLapok[i].lappontok[j]].x, torusDone[torusLapok[i].lappontok[j]].y);
+					glVertex2f(VazaDone[VazaLapok[i].lappontok[j]].x, VazaDone[VazaLapok[i].lappontok[j]].y);
 				}
 				glEnd();
 				
@@ -164,116 +179,75 @@ void rendezverajzoltoruszt(int i){
 }
 
 void rendezes() {
-	sort(torusLapok.begin(), torusLapok.end(), tavolsagszerint);
-	for (int i = 0; i < torusLapok.size(); i++) {
-		rendezverajzoltoruszt(i);		
+	sort(VazaLapok.begin(), VazaLapok.end(), tavolsagszerint);
+	for (int i = 0; i < VazaLapok.size(); i++) {
+		rendezverajzolVazat(i);		
 	}
 }
 
 
 
-void kirajzol(){
-
-	/*for (int i = 0; i < 8; i++) {
-		vec4 pointH = ihToH(kockaElek[i]);
-		vec4 transformedPoint = coordinateTraszfMatrix*pointH;
-		kockaTrUtan[i] = transformedPoint;
-	}*/
-	
-	/*vec3 ab, ac;
-	for (int i = 0; i < kockalapok.size(); i++) {
-		kockalapok[i].isKockaLap = true;
-		int b = kockalapok[i].lappontok[1], a = kockalapok[i].lappontok[0], c = kockalapok[i].lappontok[3], d = kockalapok[i].lappontok[2];
-		vec3 aV = hToIh(kockaTrUtan[a]), bV = hToIh(kockaTrUtan[b]), cV = hToIh(kockaTrUtan[c]), dV = hToIh(kockaTrUtan[d]);
-		ab = bV - aV;
-		ac = cV - aV;
-
-
-		vec3 PC = { 0 - kockaTrUtan[a].x, 0 - kockaTrUtan[a].y, centrumpont - kockaTrUtan[a].z };
-		
-		kockalapok[i].normal = cross(normalize(ab), normalize(ac));
-		
-		kockalapok[i].skalaris = dot(normalize(kockalapok[i].normal), normalize(PC));
-
-		vec4 temp = ihToH(fenyVektor);
-		
-		vec4 tempf = transpose(inverse(coordinateTraszfMatrix))*temp;
-
-		vec3 szinfeny = { tempf.x, tempf.y, tempf.z };
-
-		float lapszin = (dot(normalize(kockalapok[i].normal), normalize(szinfeny)) + 1.0f) / 2.0f;
-		
-		kockalapok[i].lapszin = lapszin;
-
-		kockalapok[i].sulypont = (aV + bV + cV + dV) / 4;
-
-		vec3 centrum = { 0,0,centrumpont };
-
-		kockalapok[i].tavolsag = dist(centrum, kockalapok[i].sulypont);
-	}
-
-	for (int i = 0; i < 8; i++) {
-		vec4 pointH = kockaTrUtan[i];
-		vec4 transformedPoint = w2v *vetites*pointH;
-		if (transformedPoint.w != 0)
-		{
-			vec3 res = hToIh(transformedPoint);
-			if (res.z == 0) {
-				rajzolhatoKocka[i] = { res.x, res.y };
-			}
-		}
-	}*/
-	
+void kirajzol(){	
 	mat4 temp = coordinateTraszfMatrix*forgatas;
-	torusDone.clear();
-	transzormaltTorus.clear();
-	for (int i = 0; i < torus.size(); i++) {
-		vec4 ph = ihToH(torus[i]);
+	VazaDone.clear();
+	transzormaltVaza.clear();
+	for (int i = 0; i < Vaza.size(); i++) {
+		vec4 ph = ihToH(Vaza[i]);
 		vec4 pt = temp * ph;
-		transzormaltTorus.push_back(pt);
+		transzormaltVaza.push_back(pt);
 	}
 	
-	for (int i = 0; i < torusLapok.size(); i++) {
-		int a = torusLapok[i].lappontok[1],
-			b = torusLapok[i].lappontok[0],
-			c = torusLapok[i].lappontok[2],
-			d = torusLapok[i].lappontok[3];	
-		vec3 aV = hToIh(transzormaltTorus[a]), bV = hToIh(transzormaltTorus[b]), cV = hToIh(transzormaltTorus[c]), dV = hToIh(transzormaltTorus[d]);
+	for (int i = 0; i < VazaLapok.size(); i++) {
+		int a = VazaLapok[i].lappontok[1],
+			b = VazaLapok[i].lappontok[0],
+			c = VazaLapok[i].lappontok[2],
+			d = VazaLapok[i].lappontok[3];	
+		vec3 aV = hToIh(transzormaltVaza[a]), bV = hToIh(transzormaltVaza[b]), cV = hToIh(transzormaltVaza[c]), dV = hToIh(transzormaltVaza[d]);
 		
 		vec3 ab = bV - aV, ac = cV - aV;
 
-		vec3 PC = { 0 - transzormaltTorus[a].x, 0 - transzormaltTorus[a].y, centrumpont - transzormaltTorus[a].z };
+		vec3 PC = { 0 - transzormaltVaza[a].x, 0 - transzormaltVaza[a].y, centrumpont - transzormaltVaza[a].z };
 		
-		torusLapok[i].normal = cross(normalize(ab), normalize(ac));
+		VazaLapok[i].normal = cross(normalize(ab), normalize(ac));
 		
-		torusLapok[i].skalaris = dot(normalize(torusLapok[i].normal), normalize(PC));
-
+		VazaLapok[i].skalaris = dot(normalize(VazaLapok[i].normal), normalize(PC));
+        //if (VazaLapok[i].skalaris<=0.0) VazaLapok[i].skalaris=VazaLapok[i].skalaris*-1;
+        //std::cout<<VazaLapok[i].skalaris<<std::endl;
+        
 		vec4 temp = ihToH(fenyVektor);
 		
 		vec4 tempf = transpose(inverse(coordinateTraszfMatrix))*temp;
 		
 		vec3 szinfeny = { tempf.x, tempf.y, tempf.z };
 		
-		float lapszin = (dot(normalize(torusLapok[i].normal), normalize(szinfeny)) + 1.0f) / 2.0f;
+        float lapszin;
+        if (VazaLapok[i].skalaris<=0.0){
+            VazaLapok[i].skalaris=VazaLapok[i].skalaris*-1;
+            lapszin = 1-((dot(normalize(VazaLapok[i].normal), normalize(szinfeny)) + 1.0f) / 2.0f);
+        }
+		else{
+            lapszin = (dot(normalize(VazaLapok[i].normal), normalize(szinfeny)) + 1.0f) / 2.0f;
+        }
+        
 		
-		torusLapok[i].lapszin =lapszin;
+		VazaLapok[i].lapszin =lapszin;
 
 
-		torusLapok[i].sulypont = (aV + bV + cV + dV) / 4;
+		VazaLapok[i].sulypont = (aV + bV + cV + dV) / 4;
 
 		vec3 centrum = { 0, 0 , centrumpont };
 
-		torusLapok[i].tavolsag = dist(centrum, torusLapok[i].sulypont);
+		VazaLapok[i].tavolsag = dist(centrum, VazaLapok[i].sulypont);
 	}
 
-	for (int i = 0; i < transzormaltTorus.size(); i++) {
-		vec4 pointH = transzormaltTorus[i];
+	for (int i = 0; i < transzormaltVaza.size(); i++) {
+		vec4 pointH = transzormaltVaza[i];
 		vec4 transformedPoint = w2v *vetites*pointH;
 		if (transformedPoint.w != 0)
 		{
 			vec3 res = hToIh(transformedPoint);
 			if (res.z == 0) {
-				torusDone.push_back({ res.x, res.y });
+				VazaDone.push_back({ res.x, res.y });
 			}
 		}
 
@@ -298,10 +272,12 @@ void billentyuMuveletek() {
 		alfa -= lepeskozjobbrabalra*5;
 	}
 	if (keyStates['w'] && eye.z <= 30) {
-		eye.z += lepeskozfelle*5;
+		//eye.z += lepeskozfelle*5;
+        magassag+=0.1;
 	}
 	if (keyStates['s'] && eye.z >= -30) {
-		eye.z -= lepeskozfelle*5;
+		//eye.z -= lepeskozfelle*5;
+        magassag-=0.1;
 	}
 	if (keyStates['-']) {
 		centrumpont -= centrumpont/1000;
@@ -330,7 +306,7 @@ void display() {
 	initMatrices();
 	cameraSzamol();
 	cameraTranszformacio();
-	initTorus();
+	initVaza();
 	kirajzol();
 	rendezes();
 	glutSwapBuffers();
